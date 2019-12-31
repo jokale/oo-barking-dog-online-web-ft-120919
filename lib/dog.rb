2e00b047348211a67c03940235a031491b521efa
Joanna E K <kalemajoanna@gmail.com>
2019-12-31 18:32:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,38 @@
-# Your code goes here!
-class Dog 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Your code goes here!
+# class Dog 
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
  
-  def name
-    @this_dogs_name
-  end
+#   def name
+#     @this_dogs_name
+#   end
   
-  def bark 
-    puts "woof!"
-  end 
-end
+#   def bark 
+#     puts "woof!"
+#   end 
+# end
 
 
-fido = Dog.new 
+# fido = Dog.new 
